e79a51ddf0cdff4d4a5a84ab13748de4ca21e731
thesimalaydin <77446863+thesimalaydin@users.noreply.github.com>
2023-08-04 09:44:40 +0300

    Add files via upload

---
+++ shastaj/shastaj/shastaj.cpp
@@ -0,0 +1,33 @@
+﻿// shastaj.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include "sha1.h"
+#include <string>
+#include <iostream>
+using std::string;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    const string input = "abc";
+
+    SHA1 checksum;
+    checksum.update(input);
+    const string hash = checksum.final();
+
+    cout << "The SHA-1 of \"" << input << "\" is: " << hash << endl;
+
+    return 0;
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
